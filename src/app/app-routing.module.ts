c33b3027713cbc70c839a1710359ab43cb26e557
willyanlz <willyangenaro4321@gmail.com>
2023-04-28 02:04:40 -0300

    Adicionado dist, server.js...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { CreateUserComponent } from './create';
+import { UpdateUserComponent } from './update';
+import { DeleteUserComponent } from './delete';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo: "", pathMatch: "full"},
+  {path: "users", component: UsuariosComponent},
+  {path: "users/create", component: CreateUserComponent},
+  {path: "users/update/:id", component: UpdateUserComponent},
+  {path: "users/delete/:id", component: DeleteUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { CommonModule, DatePipe } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios';
+import { UsuarioService } from './usuarios/services';
+import { CreateUserComponent } from './create';
+import { DataBrPipe } from './usuarios/pipes';
+import { UpdateUserComponent } from './update';
+import { DeleteUserComponent } from './delete';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    CreateUserComponent,
+    DataBrPipe,
+    UpdateUserComponent,
+    DeleteUserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [UsuarioService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
